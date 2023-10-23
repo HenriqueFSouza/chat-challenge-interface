c521e5604f414d4c5e412472096882d10cea93f5
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2023-10-23 15:27:28 -0300

    add headers configuration

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 const chatApi = axios.create({
   baseURL: 'https://chat-challenge-api.onrender.com',
+  headers: {
+    'Content-Type': 'application/json',
+    "Accept": "application/json ",
+  },
   withCredentials: true,
 })
 
