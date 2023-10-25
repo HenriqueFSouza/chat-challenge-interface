a1ea07b413d891f67f586e125e03df9bd86f9adf
Henrique Francisco de Souza <henriquesouza432@outlook.com>
2023-10-25 09:32:47 -0300

    fixed baseurl as localhost

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const chatApi = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://chat-challenge-api.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json ',
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    'Access-Control-Allow-Origin': 'https://chat-challenge-api.onrender.com/',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
     'Access-Control-Allow-Credentials': true,
     'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With',
